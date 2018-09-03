a81f669a58d3dcd5f02c69d19865887926d7fa38
romanarranz <burial92@gmail.com>
2018-09-03 22:39:10 +0200

    houses model

---
+++ src/models/houses/schema.ts
@@ -0,0 +1,44 @@
+// Package.
+import { Schema, SchemaDefinition, SchemaOptions } from 'mongoose';
+
+// Code.
+const definitions: SchemaDefinition = {
+  bathroomNumber: { type: Number, required: true },
+  bedroomNumber: { type: Number, required: true },
+  catSpaces: { type: Number, required: true },
+  comission: { type: Number, required: true },
+  constructionYear: { type: Number, required: true },
+  datasourceName: { type: String, required: true },
+  floor: { type: Number, required: true },
+  imgHeight: { type: Number, required: true },
+  imgUrl: { type: String, required: true },
+  imgWidth: { type: Number, required: true },
+  keywords: { type: String, required: true },
+  latitude: { type: Number, required: true },
+  listerName: { type: String, required: true },
+  listerUrl: { type: String, required: true },
+  listingType: { type: String, required: true },
+  locationAccuracy: { type: Number, required: true },
+  longitude: { type: Number, required: true },
+  price: { type: Number, required: true },
+  priceCurrency: { type: String, required: true },
+  priceFormatted: { type: String, required: true },
+  priceHight: { type: Number, required: true },
+  priceLow: { type: Number, required: true },
+  propertyType: { type: String, required: true },
+  roomNumber: { type: Number, required: true },
+  size: { type: Number, required: true },
+  sizeType: { type: String, required: true },
+  summary: { type: String, required: true },
+  thumbHeight: { type: Number, required: true },
+  thumbUrl: { type: String, required: true },
+  thumbWidth: { type: Number, required: true },
+  title: { type: String, required: true },
+  updatedInDays: { type: Number, required: true },
+  updatedOmDaysFormatted: { type: String, required: true },
+};
+const options: SchemaOptions = {
+  timestamps: true,
+};
+
+export const schema: Schema = new Schema(definitions, options);
---
+++ src/models/houses/types.ts
@@ -0,0 +1,43 @@
+// Package.
+import { Document } from 'mongoose';
+
+// Code.
+export interface HouseType {
+  bathroomNumber: number;
+  bedroomNumber: number;
+  catSpaces: number;
+  comission: number;
+  constructionYear: number;
+  datasourceName: string;
+  floor: number;
+  imgHeight: number;
+  imgUrl: string;
+  imgWidth: number;
+  keywords: string;
+  latitude: number;
+  listerName: string;
+  listerUrl: string;
+  listingType: string;
+  locationAccuracy: number;
+  longitude: number;
+  price: number;
+  priceCurrency: string;
+  priceFormatted: string;
+  priceHight: number;
+  priceLow: number;
+  propertyType: string;
+  roomNumber: number;
+  size: number;
+  sizeType: string;
+  summary: string;
+  thumbHeight: number;
+  thumbUrl: string;
+  thumbWidth: number;
+  title: string;
+  updatedInDays: number;
+  updatedOmDaysFormatted: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export interface HouseDocument extends HouseType, Document {}
